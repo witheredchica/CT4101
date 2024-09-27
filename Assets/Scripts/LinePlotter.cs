56fd00c374c597b3540c74bce8ddc09ecb185d2d
jack hawker <151658059+witheredchica@users.noreply.github.com>
2024-09-27 12:53:25 +0100

    2.0
    added circle maker and funky mode

--- Assets/Scripts/LinePlotter.cs
+++ Assets/Scripts/LinePlotter.cs
@@ -15,6 +15,7 @@ public class LinePlotter : MonoBehaviour {
     public int p = 1;
 
     public bool isCircle = false;
+    public bool funkyMode = false;
 
     private void OnDrawGizmos() {
         //if there are points in the list
@@ -23,6 +24,8 @@ public class LinePlotter : MonoBehaviour {
             Gizmos.color = Color.green;
             //draw a line between each point in the list
             for (int i = 0; i < points.Count - 1; i++) {
+                //rotate hue each point
+                Gizmos.color = Color.HSVToRGB((1.0f/points.Count)*i, 1, 1);
                 Gizmos.DrawLine(points[i], points[i + 1]);
             }
         }
@@ -34,6 +37,8 @@ public class LinePlotter : MonoBehaviour {
 
     private float CalcCircleY(float x, float x1, float c) {
         float ySqr = c - ((x - x1) * (x - x1));
+
+        //stops the script from trying to square root a negative value
         float y = (ySqr >= 0) ? Mathf.Pow(ySqr, 0.5f) : -1 * Mathf.Pow(-ySqr, 0.5f);
         return y;
     }
@@ -58,12 +63,20 @@ public class LinePlotter : MonoBehaviour {
             switch (isCircle) {
                 case true:
                     points.Add(new Vector3(xPos, CalcCircleY(xPos, x1, c), 0f));
+                    if (funkyMode) {
+                        points.Add(new Vector3(xPos, -1 * CalcCircleY(xPos, x1, c), 0f));
+                    }
                     break;
                 case false:
                     points.Add(new Vector3(xPos, CalcY(xPos, x1, y1, m, c, p), 0f));
                     break;
             }
-            
+        }
+
+        if (isCircle && !funkyMode) {
+            for (float xPos = -1 * x; xPos > x; xPos -= 0.2f) {
+                points.Add(new Vector3(xPos, -1 * CalcCircleY(xPos, x1, c), 0f));
+            }
         }
         Debug.Log("Repopulating gizmo positions");
     }
